5eeaad84481489eeec4d3190b4d47e17129cd698
Matthias Piepkorn <mpiepk@gmail.com>
2017-08-01 15:11:49 +0200

    Performance optimization

--- Assets/Scripts/Bezier3D2.cs
+++ Assets/Scripts/Bezier3D2.cs
@@ -10,18 +10,14 @@ public class Bezier3D2 : MonoBehaviour {
     public float thickness = 0.25f;
     public float breadth = 0.25f;
     public Vector3[] points = new Vector3[4];
-    private List<GameObject> _waypoints;
-    public GameObject waypointObj;
-
-    public List<GameObject> Waypoints {
-        get { return _waypoints; }
-    }
+    private List<Vector3> controlPoints;
 
 #if UNITY_EDITOR
     // instead of @script ExecuteInEditMode()
     [ContextMenu("Update mesh")]
     public void UpdateMesh() {
         Debug.Log("Constructing sQuad from ContextMenu");
+        controlPoints = AdaptControlPoints();
         var mesh = CreateMesh();
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshCollider>().sharedMesh = mesh;
@@ -29,51 +25,16 @@ public class Bezier3D2 : MonoBehaviour {
 #endif
 
     private void OnDrawGizmos() {
+        controlPoints = AdaptControlPoints();
         Gizmos.color = Color.blue;
         var waypointsVects = GenerateWaypoints();
         foreach (var wp in waypointsVects) {
             Gizmos.DrawWireSphere(wp + transform.position, 0.5f);
         }
-
-//        var controlPoints = AdaptControlPoints();
-//
-//        Gizmos.color = Color.red;
-//        foreach (var cp in controlPoints) {
-//            Gizmos.DrawWireSphere(cp + transform.position, 0.25f);
-//        }
-//        Vector3 upNormal = new Vector3(0, -1, 0);
-//        float width = breadth / 2f;
-//        float height = thickness / 2f;
-//        float scaling = 1;
-//
-//        for (int s = 0; s < resolution; s++) {
-//            float t0 = ((float) s) / resolution;
-//            float t1 = ((float) s + 1) / resolution;
-//
-//            Vector3 p0 = PointOnPath(t0, controlPoints) + transform.position;
-//            Vector3 p1 = PointOnPath(t1, controlPoints) + transform.position;
-//
-//            Gizmos.color = Color.green;
-//            Gizmos.DrawLine(p0, p1);
-//
-//            Vector3 dir1 = PointOnPath(Math.Max(0, t0 - 1f/resolution), controlPoints) - PointOnPath(Math.Min(1, t0 + 1f/resolution), controlPoints);
-//            Vector3 dir2 = PointOnPath(Math.Max(0, t1 - 1f/resolution), controlPoints) - PointOnPath(Math.Min(1, t1 + 1f/resolution), controlPoints);
-//            dir1.Normalize();
-//            dir2.Normalize();
-//            Vector3 right1 = Vector3.Cross(upNormal, dir1) * width;
-//            Vector3 right2 = Vector3.Cross(upNormal, dir2) * width;
-//
-//            Gizmos.color = Color.red;
-//            Gizmos.DrawLine(p0, p0 + right1);
-//            Gizmos.DrawLine(p0, p0 - right1);
-//            Gizmos.DrawLine(p0 + right1, p1 + right2);
-//            //Gizmos.DrawLine(p1 + right1, p2 + right2);
-//            Gizmos.DrawLine(p0 - right1, p1 - right2);
-//            //Gizmos.DrawLine(p1 - right1, p2 - right2);
-//        }
     }
 
     private void OnDrawGizmosSelected() {
+        controlPoints = AdaptControlPoints();
         Gizmos.color = Color.green;
         foreach (var p in points) {
             Gizmos.DrawWireSphere(p + transform.position, 0.5f);
@@ -85,83 +46,35 @@ public class Bezier3D2 : MonoBehaviour {
     }
 
     private List<Vector3> AdaptControlPoints() {
-        List<Vector3> controlPoints = new List<Vector3>();
+        List<Vector3> controls = new List<Vector3>();
         //generate the end and control points
         for (int i = 0; i < points.Length; i++) {
             if ((i > 2 && i < points.Length - 1 && (i + 1) % 2 == 0 && points.Length > 4) ||
                 (i == points.Length - 1 && i > 3 && i % 2 == 0)) {
-                controlPoints.Add((points[i - 1] + points[i]) / 2f);
+                controls.Add((points[i - 1] + points[i]) / 2f);
             }
-            controlPoints.Add(points[i]);
+            controls.Add(points[i]);
         }
-        return controlPoints;
+        return controls;
     }
 
-    public void Start() {
+    public void Awake() {
 //        var mesh = CreateMesh();
 //        GetComponent<MeshFilter>().mesh = mesh;
 //        GetComponent<MeshCollider>().sharedMesh = mesh;
-        _waypoints = GenerateWaypointObjs();
+        controlPoints = AdaptControlPoints();
     }
 
     private List<Vector3> GenerateWaypoints() {
         List<Vector3> waypoints = new List<Vector3>();
-        var controlPoints = AdaptControlPoints();
         for (int s = 0; s < waypointResolution; s++) {
             float t = ((float) s) / waypointResolution;
-            waypoints.Add(PointOnPath(t, controlPoints));
+            waypoints.Add(BezierMath.PointOnPath(t, controlPoints));
         }
         return waypoints;
-    }
-
-    private List<GameObject> GenerateWaypointObjs() {
-        List<GameObject> waypoints = new List<GameObject>();
-        var controlPoints = AdaptControlPoints();
-        for (int s = 0; s < waypointResolution; s++) {
-            float t = ((float) s) / waypointResolution;
-            Vector3 dir = TangentAt(t, controlPoints);
-            var p = PointOnPath(t, controlPoints);
-            var o = Instantiate(waypointObj, transform, false);
-            o.transform.localPosition = p;
-            o.transform.localRotation = Quaternion.LookRotation(dir, Vector3.up);
-            waypoints.Add(o);
-        }
-        return waypoints;
-    }
-
-    //cacluates point coordinates on a quadratic curve
-    public static Vector3 PointOnPath(float t, List<Vector3> controlPoints) {
-        int bezierCount = (controlPoints.Count - 1) / 3;
-        int nBezier = Math.Min((int) (t * bezierCount), bezierCount - 1);
-        int startIdx = nBezier * 3;
-        Vector3 p0 = controlPoints[startIdx];
-        Vector3 p1 = controlPoints[startIdx + 1];
-        Vector3 p2 = controlPoints[startIdx + 2];
-        Vector3 p3 = controlPoints[startIdx + 3];
-
-        t = (t - (nBezier * 1f / bezierCount)) * bezierCount;
-
-        float u, uu, uuu, tt, ttt;
-        Vector3 p;
-
-        u = 1 - t;
-        uu = u * u;
-        uuu = uu * u;
-
-        tt = t * t;
-        ttt = tt * t;
-
-        p = uuu * p0;
-        p += 3 * uu * t * p1;
-        p += 3 * u * tt * p2;
-        p += ttt * p3;
-
-        return p;
     }
 
     public Mesh CreateMesh() {
-        var controlPoints = AdaptControlPoints();
-
         Mesh mesh;
 
         mesh = new Mesh();
@@ -184,11 +97,11 @@ public class Bezier3D2 : MonoBehaviour {
             float t0 = ((float) s) / resolution;
             float t1 = ((float) s + 1) / resolution;
 
-            Vector3 p0 = PointOnPath(t0, controlPoints);
-            Vector3 p1 = PointOnPath(t1, controlPoints);
+            Vector3 p0 = BezierMath.PointOnPath(t0, controlPoints);
+            Vector3 p1 = BezierMath.PointOnPath(t1, controlPoints);
 
-            Vector3 dir1 = TangentAt(t0, controlPoints);
-            Vector3 dir2 = TangentAt(t1, controlPoints);
+            Vector3 dir1 = BezierMath.TangentAt(t0, controlPoints);
+            Vector3 dir2 = BezierMath.TangentAt(t1, controlPoints);
             dir1.Normalize();
             dir2.Normalize();
             Vector3 right1 = -Vector3.Cross(upNormal, dir1).normalized * width;
@@ -263,30 +176,30 @@ public class Bezier3D2 : MonoBehaviour {
         return mesh;
     }
 
-    private Vector3 TangentAt(float t0, List<Vector3> controlPoints) {
-        return PointOnPath(Math.Max(0, t0 + 1f/resolution), controlPoints) - PointOnPath(Math.Min(1, t0 - 1f/resolution), controlPoints);
+    public Vector3 TangentAt(float t) {
+        return BezierMath.TangentAt(t, controlPoints);
     }
 
-    public Vector3 GetPointAt(float t) {
-        return PointOnPath(Math.Min(Math.Max(0, t), 1f), AdaptControlPoints()) + transform.position;
+    public Vector3 GetPointAt(float t, float offset = 0) {
+        Vector3 p = BezierMath.PointOnPath(Mathf.Clamp(t, 0, 1f), controlPoints);
+        Vector3 dir = TangentAt(t);
+        return transform.TransformPoint(p + Vector3.Cross(dir, Vector3.up).normalized * offset);
     }
 
     public void UpdateTransformTo(Transform tf, float t, float offset = 0) {
-        var controlPoints = AdaptControlPoints();
-        Vector3 p = PointOnPath(t % 1f, controlPoints);
-        Vector3 dir = TangentAt(t, controlPoints);
-        tf.position = transform.TransformPoint(p + Vector3.Cross(dir, Vector3.up).normalized * offset);
+        Vector3 transformedPoint = GetPointAt(t, offset);
+        Vector3 dir = TangentAt(t);
+        tf.position = transformedPoint;
         tf.rotation = Quaternion.LookRotation(dir, Vector3.up);
     }
 
     public float GetProgress(Vector3 position) {
-        var controlPoints = AdaptControlPoints();
         Vector3 objPos = transform.InverseTransformPoint(position);
-        float minMag = (PointOnPath(0, controlPoints) - objPos).sqrMagnitude;
+        float minMag = (BezierMath.PointOnPath(0, controlPoints) - objPos).sqrMagnitude;
         float minT = 0;
         for (int s = 0; s < waypointResolution; s++) {
             float t = ((float) s) / waypointResolution;
-            var mag = (PointOnPath(t, controlPoints) - objPos).sqrMagnitude;
+            var mag = (BezierMath.PointOnPath(t, controlPoints) - objPos).sqrMagnitude;
             if (mag < minMag) {
                 minT = t;
                 minMag = mag;
---
+++ Assets/Scripts/BezierMath.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public abstract class BezierMath {
+    private static float TANGENT_RES = 10000f;
+
+    //cacluates point coordinates on a list of quadratic curves
+    public static Vector3 PointOnPath(float t, List<Vector3> controlPoints) {
+        Assert.AreEqual(0, (controlPoints.Count - 1) % 3, "must be a list of cubic beziers sharing start and end, plus a start point");
+        int bezierCount = (controlPoints.Count - 1) / 3;
+        int nBezier = Math.Min((int) (t * bezierCount), bezierCount - 1);
+        int startIdx = nBezier * 3;
+        Vector3 p0 = controlPoints[startIdx];
+        Vector3 p1 = controlPoints[startIdx + 1];
+        Vector3 p2 = controlPoints[startIdx + 2];
+        Vector3 p3 = controlPoints[startIdx + 3];
+
+        t = (t - (nBezier * 1f / bezierCount)) * bezierCount;
+
+        float u, uu, uuu, tt, ttt;
+        Vector3 p;
+
+        u = 1 - t;
+        uu = u * u;
+        uuu = uu * u;
+
+        tt = t * t;
+        ttt = tt * t;
+
+        p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+
+        return p;
+    }
+
+    public static Vector3 TangentAt(float t, List<Vector3> controlPoints) {
+        return PointOnPath(Math.Max(0, t + 1f/TANGENT_RES), controlPoints) - PointOnPath(Math.Min(1, t - 1f/TANGENT_RES), controlPoints);
+    }
+
+}
